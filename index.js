9f31342066664c110987c886fafeb8fdee7143f3
Collins Kathurima <collinskathu313@gmail.com>
2024-10-01 17:45:39 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {name:'Collins',
+     streetAddress:'Komarock',
+     }
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const copyOfEmployee={...employee,"streetAddress":"11 Broadway"}
+    return copyOfEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee["streetAddress"]="12 Broadway"
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const copyOfEmployee={...employee}
+    delete copyOfEmployee[key]
+    return copyOfEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
+
+
+
+
